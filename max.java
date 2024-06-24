f021f0114c86b27e5c4b34b05b59beddf82c4d4c
Navesh <navesh.professional@gmail.com>
2024-06-25 00:45:38 +0530

    Math.max method

---
+++ max.java
@@ -0,0 +1,11 @@
+public class max {
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        //int d=42;
+        int c=Math.max(a,b);
+        System.out.println("max is "+c);
+    }
+    
+}
